80688d8a64c2f0db614dfe9cfae2a0eafbd77940
Melissa Quintanilla <last210@gmail.com>
2015-11-09 21:38:36 -0600

    more changes

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -3,13 +3,16 @@ import re
 import sys
 sys.tracebacklimit=0
 
-user_input = raw_input("Enter webaddress to be scraped: ")
-try:
-	urllib.urlopen(user_input)
-except:
-	print "You did not enter a valid URL"
+if __name__ == "__main__":
+	user_input = raw_input("Enter URL to be scraped: ")
+
+def main():	
+	try:
+		urllib.urlopen(user_input)
+	except:
+		print "You did not enter a valid URL"
 	
-def print_html(html):
+def print_html(user_input):
 	web_input = urllib.urlopen(user_input)
 	web_output = web_input.read()
 	print web_output
