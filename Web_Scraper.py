6fef021fbc7b550fdf59fa2425ed947763f1b13c
last210 <last210@gmail.com>
2015-12-11 13:57:19 -0600

    valid_stock

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import urllib
 import re
 import logging
@@ -70,67 +70,3 @@ if __name__ == "__main__":
     # main()
     # get_current_stock()
     valid_stock()
-=======
-import urllib
-import re
-import logging
-
-logger = logging.getLogger(__name__)
-logger.info('info log')
-logger.debug('debug log')
-
-
-def configure_logging():
-    root_logger = logging.getLogger()
-    log_formatter = logging.Formatter(
-        "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        "%Y-%m-%d %H:%M:%S")
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
-    root_logger.addHandler(console_handler)
-    root_logger.setLevel(logging.DEBUG)
-
-
-def print_html(user_input):
-    web_input = urllib.urlopen(user_input)
-    web_output = web_input.read()
-    print web_output
-
-
-def main():
-    configure_logging()
-    user_input = raw_input("Enter URL to be scraped: ")
-    logger.info("user input is %s", user_input)
-
-    try:
-        urllib.urlopen(user_input)
-    except:
-        logger.exception("URL exception")
-        print "You did not enter a valid URL"
-
-    print_html(user_input)
-
-
-def get_current_stock():
-    open_stockfile = open("stocks.txt")
-    stockfile = open_stockfile.read()
-    user_stock = raw_input("Which stock would you like the current price of? ")
-
-    if user_stock.upper() in stockfile:
-        url = "http://finance.yahoo.com/q?s=" + user_stock + "&ql=1"
-        open_url = urllib.urlopen(url)
-        read_url = open_url.read()
-        find_price = ('<span id="yfs_l84_' +
-                      user_stock.lower() + '">(.+?)</span>')
-        compile_price = re.compile(find_price)
-        price = re.findall(compile_price, read_url)
-        print ("The current stock price of {0} is {1}".format(
-               user_stock.upper(), price))
-    else:
-        print "You did not enter a valid stock"
-
-
-if __name__ == "__main__":
-    # main()
-    get_current_stock()
->>>>>>> d093fc8b78358c005f91c842cf244696ae5daf0a
